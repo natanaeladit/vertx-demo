66444913b55ef225f12efb4f7f9218406c9287f9
NatanaelA <natanaela@xrvision.com>
2019-03-26 22:48:00 +0800

    add db lib

--- src/main/kotlin/VertxApp.kt
+++ src/main/kotlin/VertxApp.kt
@@ -6,19 +6,14 @@ object VertxApp {
     fun main(args: Array<String>) {
         val vertx = Vertx.vertx()
         var server = vertx.createHttpServer()
-
         var router = Router.router(vertx)
-
         router.route().handler({ routingContext ->
-
             // This handler will be called for every request
             var response = routingContext.response()
             response.putHeader("content-type", "text/plain")
-
             // Write to the response and end it
             response.end("Hello World from Vert.x-Web!")
         })
-
         server.requestHandler(router).listen(8888)
     }
 }
